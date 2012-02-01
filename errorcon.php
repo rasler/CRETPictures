4a12d0dbcbb404baa335fe2f4f3df817c96b6d9e
Estelle Volckringer <estelle.volckringer@gmail.com>
2012-02-02 00:38:01 +0100

    module de connexion et de déconnexion

---
+++ errorcon.php
@@ -0,0 +1,17 @@
+<?php include("structure.php"); ?>
+
+<span class ="errorcon">  
+    <form method="post" enctype="multipart/form-data" action="conex.php">
+        <fieldset>
+            <legend>Connexion</legend> <!-- Titre du fieldset -->
+
+            <label for="nom">Login ...... :  </label>
+            <input type="text" name="nom" id="nom" tabindex="10" /><br />
+
+            <label for="mot_passe">Mot de passe :  </label>
+            <input type="password" name="mot_passe" id="mot_passe" tabindex="30" /><br />
+
+            <input type="submit" tabindex="40"><input type="reset" tabindex="50">
+        </fieldset>	
+    </form>
+</span>  
